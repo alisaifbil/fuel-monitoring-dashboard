0a30df6b1fc6de9e863dabc0299b0450e9185a96
Ali Saif Bilgrami <alisaifbil@gmail.com>
2023-08-08 18:02:59 +0500

    adding chart to display fuel consumption over time

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import VehicleSummary from "@/components/VehicleSummary";
 import RecentRefills from "@/components/RecentRefills";
+import DetailsOverChart from "@/components/DetailsOverChart";
 
 const Dashboard = () => {
   return (
-    <div className="w-full flex flex-row">
-      <div className="grid grid-cols-2 gap-y-4 ml-[1.5rem] w-[65%]">
-        <VehicleSummary />
-        <VehicleSummary />
-        <VehicleSummary />
-        <VehicleSummary />
+    <>
+      <div className="w-full flex flex-row">
+        <div className="grid grid-cols-2 gap-y-4 ml-[1.5rem] w-[65%]">
+          <VehicleSummary />
+          <VehicleSummary />
+          <VehicleSummary />
+          <VehicleSummary />
+        </div>
+        <div className="w-[40%] mr-[1rem] h-full">
+          <RecentRefills />
+        </div>
       </div>
-      <div className="w-[40%] mr-[1rem] h-full">
-        <RecentRefills />
+      <div className="h-max w-[32vw]">
+        <DetailsOverChart />
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ components/DetailsOverChart.jsx
@@ -0,0 +1,54 @@
+"use client"
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+// import faker from 'faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => Math.random() * (1000 - 0) + 0),
+      backgroundColor: 'rgba(1, 0, 255, 0.5)',
+    }
+  ],
+};
+
+const DetailsOverChart = () => {
+  return <Bar options={options} data={data} />;
+}
+
+export default DetailsOverChart;
