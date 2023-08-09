cd7db88fcbacc12c8986be5772112fffefa4d208
Ali Saif Bilgrami <alisaifbil@gmail.com>
2023-08-09 21:17:08 +0500

    amending styles to add responsiveness to sidebar and navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,18 +1,43 @@
 import React from "react";
 
+import Link from "next/link";
 import Image from "next/image";
 
 const NavBar = () => {
+  const Menus = [{ title: "Dashboard" }, { title: "Entry" }];
   return (
-    <nav className="flex-between w-full mb-16 top-0 pt-3 justify-center h-[3.25rem] border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl">
-      <div className="float-right pr-16 pt-3">
-        <Image
-          src={"/next.svg"}
-          width={37}
-          height={37}
-          className="rounded-lg object-contain"
-          alt="profile"
-        />
+    <nav className="w-auto h-[3.25rem] border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl">
+      <div className="flex md:float-right py-6 pr-[2.5rem] top-0 justify-between">
+        <div className="block md:hidden md:w-auto">
+          <ul class="flex p-0 flex-row space-x-4 mt-0 border-0 md:hidden">
+            <li>
+              <a
+                href="#"
+                class="bg-transparent text-blue-700 p-0"
+                aria-current="page"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                class="hover:bg-gray-100 hover:bg-transparent border-0 hover:text-blue-700 p-0"
+              >
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <Image
+            src={"/next.svg"}
+            width={37}
+            height={37}
+            className="rounded-lg object-contain"
+            alt="profile"
+          />
+        </div>
       </div>
     </nav>
   );
--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -14,49 +14,51 @@ const SideBar = ({ children }) => {
 
   return (
     <div className="flex">
-      <div
-        className={`${
-          show ? "w-72" : "w-20"
-        } p-5 pt-8 duration-300 h-screen bg-dark-purple relative z-20`}
-      >
-        <ChevronLeftIcon
-          className={`absolute cursor-pointer rounded-full -right-3 top-9 w-7 border-2 border-dark-purple fill-dark-purple bg-white ${
-            !show && "rotate-180"
-          }`}
-          onClick={show ? close : open}
-        />
-        <div className="flex gap-x-4 items-center">
-          <img
-            src="/next.svg"
-            className={`cursor-pointer duration-500 w-16 h-16 `}
-          />
-          <h1
-            className={`text-white origin-left font-medium text-xl duration-300 ${
-              !show && "scale-0"
+      <div className="hidden md:block">
+        <div
+          className={`${
+            show ? "w-72" : "w-20"
+          } p-5 pt-8 duration-300 h-screen bg-dark-purple relative z-20`}
+        >
+          <ChevronLeftIcon
+            className={`absolute cursor-pointer rounded-full -right-3 top-9 w-7 border-2 border-dark-purple fill-dark-purple bg-white ${
+              !show && "rotate-180"
             }`}
-          >
-            Description
-          </h1>
-        </div>
-        <ul className="pt-6">
-          {Menus.map((menu, index) => (
-            <Link key={index} href={`/${menu.title.toLowerCase()}`}>
-              <li
-                key={index}
-                className={`${
-                  show && active === menu.title && "bg-light-white"
-                } text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white rounded-md checked:bg-violet-700`}
-                onClick={() => setActive(menu.title)}
-              >
-                <span
-                  className={`${!show && "hidden"} origin-left duration-200`}
+            onClick={show ? close : open}
+          />
+          <div className="flex gap-x-4 items-center">
+            <img
+              src="/next.svg"
+              className={`cursor-pointer duration-500 w-16 h-16 `}
+            />
+            <h1
+              className={`text-white origin-left font-medium text-xl duration-300 ${
+                !show && "scale-0"
+              }`}
+            >
+              Description
+            </h1>
+          </div>
+          <ul className="pt-6">
+            {Menus.map((menu, index) => (
+              <Link key={index} href={`/${menu.title.toLowerCase()}`}>
+                <li
+                  key={index}
+                  className={`${
+                    show && active === menu.title && "bg-light-white"
+                  } text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white rounded-md checked:bg-violet-700`}
+                  onClick={() => setActive(menu.title)}
                 >
-                  {menu.title}
-                </span>
-              </li>
-            </Link>
-          ))}
-        </ul>
+                  <span
+                    className={`${!show && "hidden"} origin-left duration-200`}
+                  >
+                    {menu.title}
+                  </span>
+                </li>
+              </Link>
+            ))}
+          </ul>
+        </div>
       </div>
       <div className="z-10 text-2xl font-semibold flex-1 h-screen">
         <NavBar />
