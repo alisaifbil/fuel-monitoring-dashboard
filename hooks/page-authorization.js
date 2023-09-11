ff6365c7fcd31759750e8eed4155ab67c265d32f
Ali Saif Bilgrami <alisaifbil@gmail.com>
2023-09-11 18:25:10 +0500

    fix/ correction in page authorization logic

--- hooks/page-authorization.js
+++ hooks/page-authorization.js
@@ -1,36 +1,35 @@
-import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 
 export const pageAuthorization = (page) => {
-  const [isAuthorized, setIsAuthorized] = useState(false);
   const { data: session } = useSession();
-  useEffect(() => {
-    const populateMenu = async () => {
-      const response = await fetch(`/api/admindetails/lovdetails/sideBarMenu`, {
-        next: { revalidate: 3600 },
-      });
-      const data = await response.json();
 
-      const menuBtnList = session?.user.roles
-        ? data[0].values.filter(
-            (page) =>
-              session?.user.roles.findIndex((role) => role === page.role) !== -1
-          )
-        : [{ title: "Dashboard", role: "all_users" }];
+  const confirmAuthorization = async () => {
+    const response = await fetch(`/api/admindetails/lovdetails/sideBarMenu`, {
+      next: { revalidate: 3600 },
+    });
+    const data = await response.json();
 
-      if (
-        menuBtnList.findIndex(
-          (menu) => menu.title.toLowerCase() === page.toLowerCase()
-        ) !== -1
-      )
-        setIsAuthorized(true);
+    const menuBtnList = session?.user.roles
+      ? data[0].values.filter(
+          (page) =>
+            session?.user.roles.findIndex((role) => role === page.role) !== -1
+        )
+      : [{ title: "Dashboard", role: "all_users" }];
+    console.log(menuBtnList, "--", page.split("/")[1]);
+    const autho = menuBtnList.findIndex(
+      (menu) => menu.title.toLowerCase() === page.split("/")[1].toLowerCase()
+    );
+    console.log(autho);
+    if (
+      menuBtnList.findIndex(
+        (menu) => menu.title.toLowerCase() === page.split("/")[1].toLowerCase()
+      ) !== -1
+    )
+      return true;
 
-      setIsAuthorized(false);
-    };
-
-    populateMenu();
-  }, [session]);
-
+    return false;
+  };
+  const isAuthorized = confirmAuthorization();
   return isAuthorized;
 };
 
